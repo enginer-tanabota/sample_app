5de63d6bd9b3d016fb493f0c02b8155563d46048
enginer-tanabota <123060704+enginer-tanabota@users.noreply.github.com>
2023-06-11 07:21:11 +0000

    Finish Ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,50 @@
 class UsersController < ApplicationController
+
+  # => /users
+  def index
+    # => GET app/views/users/index.html.erb
+  end
+
+  # => /users/:id
+  def show
+    # @user = User.first
+    @user = User.find(params[:id])
+    foo = "foobar"
+    hoge = "hogehoge"
+    # debugger
+    # => GET app/views/users/show.html.erb
+  end
+
+  # /users/new
   def new
+    @user = User.new
     # => GET app/views/users/new.html.erb
   end
+
+  # => POST /users
+  def create
+    # Sign-up Param => params => user => user.save
+    @user = User.new(user_params)
+    if @user.save
+      # => Success / alert-success
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user # => GET /users/:id
+
+    else
+      # => Failure
+      render 'new', status: :unprocessable_entity
+    end
+
+  end
+
+
+  private
+
+  # Strong Parameters
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                :password_confirmation)
+  end
+
+  
 end
