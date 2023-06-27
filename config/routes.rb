07bc0d6378c1538effdc04ca722d62744d74fb2d
tanabota <tanabota.games2110@gmail.com>
2023-06-27 07:16:02 +0000

    Add Password Reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   # get 'users/new'
   # GET / => static_pages#home
   root   "static_pages#home"
@@ -24,6 +26,7 @@ Rails.application.routes.draw do
   # ...
 
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
 
 
