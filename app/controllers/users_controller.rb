13e0b1936fb1902cad62553f09b8c97721fa729e
tanabota <tanabota.games2110@gmail.com>
2023-06-29 11:11:44 +0000

    Ch13 except ch13.4.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
   def show
     # @user = User.first
     @user = User.find(params[:id])
-    foo = "foobar"
-    hoge = "hogehoge"
+    @microposts = @user.microposts.paginate(page: params[:page])
+    # foo = "foobar"
+    # hoge = "hogehoge"
     # debugger
     # => GET app/views/users/show.html.erb
   end
@@ -78,15 +79,6 @@ class UsersController < ApplicationController
 
   # beforeフィルタ
 
-  # ログイン済みユーザーかどうか確認
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url, status: :see_other
-    end
-  end
-  
   # 正しいユーザーかどうか確認(ログイン済み（current_user != nil）であることが前提)
   def correct_user
     @user = User.find(params[:id])
